e0acf8600d50a8df23c4b530be7f9396483069de
Jacob Tovsen <jacobtovsen@Jacobs-MacBook-Pro.local>
2018-06-15 11:25:41 -0500

    created tables for entry view and project view with working sample tables in controllers

--- server/public/scripts/controllers/entry.controller.js
+++ server/public/scripts/controllers/entry.controller.js
@@ -6,4 +6,10 @@ app.controller('EntryController', function(HomeService){
         console.log('in entryIn');
     }
 
+    self.entryArray = [
+        {entry: 'Populated Database', project: 'Weekend Project', date:'6/15/18', hours: 2},
+        {entry: 'Electrical Work', project: 'Garage Build', date:'6/15/18', hours: 10},
+        {entry: 'Painted the basement', project: 'Honey-do List', date:'4/5/18', hours: 4}
+    ]
+
 });
--- server/public/scripts/controllers/project.controller.js
+++ server/public/scripts/controllers/project.controller.js
@@ -6,4 +6,9 @@ app.controller('ProjectController', function(HomeService){
         console.log('in runProjectIn');
     }
 
+    self.projectArray = [
+        {project: 'Homework', totalHours: 2.5},
+        {project: 'Garage Build', totalHours: 6},
+        {project: 'Weekend Project', totalHours: 4}
+    ];
 });
