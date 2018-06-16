5d5c3e362f5b0f9c9ece0fa67d3a017acf131593
Jacob Tovsen <jacobtovsen@Jacobs-MacBook-Pro.local>
2018-06-16 16:11:54 -0500

    working time conversion.  working to get sql to receive number with 2 decimals.

--- server/public/scripts/controllers/entry.controller.js
+++ server/public/scripts/controllers/entry.controller.js
@@ -1,7 +1,8 @@
 app.controller('EntryController', function(HomeService){
     let self = this;
     console.log('in EntryController');
-    
+    self.hoursIn = 0;
+
     self.getProject = function(){
         HomeService.serviceGetProjects()
         .then(function(){
@@ -26,7 +27,8 @@ app.controller('EntryController', function(HomeService){
     self.entryIn = function(){
         console.log('time in = ', self.startIn);
         console.log('time out = ', self.endIn);
-        
+        console.log('this is titleIn:', self.titleIn)
+        self.hoursInput();
         
         let postEntryData = {entry: self.titleIn, project: self.projectIn, date: self.dateIn, hours: self.hoursIn};
         HomeService.postEntryData = postEntryData;
@@ -37,9 +39,28 @@ app.controller('EntryController', function(HomeService){
             self.titleIn = '';
             self.projectIn = '';
             self.dateIn = '';
-            self.hoursIn = '';
+            // self.hoursIn = '';
         });
     }
+
+    self.hoursInput = function(){
+        let inHours = 60 * (self.startIn.getHours());
+        let outHours = 60 * (self.endIn.getHours());
+        let inMinutes = self.startIn.getMinutes();
+        let outMinutes = self.endIn.getMinutes();
+
+        let timeIn = inHours + inMinutes;
+        let timeOut = outHours + outMinutes;
+
+        let totalTime = ((timeOut - timeIn) / 60).toFixed(2);
+
+        console.log(inHours);
+        console.log(outHours);
+        console.log(inMinutes);
+        console.log(outMinutes);
+        console.log('Total time:', totalTime , 'hours');
+        self.hoursIn = totalTime;
+    };
 
     self.getEntry();
     self.getProject();
