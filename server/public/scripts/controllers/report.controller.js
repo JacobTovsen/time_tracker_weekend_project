f17b1c1990f18c7e0e31be8b936cd11b38e6a4cf
Jacob Tovsen <jacobtovsen@Jacobs-MacBook-Pro.local>
2018-06-16 23:11:23 -0500

    labeling report graph

--- server/public/scripts/controllers/report.controller.js
+++ server/public/scripts/controllers/report.controller.js
@@ -11,47 +11,47 @@ app.controller('ReportController', function(HomeService){
             self.pushProjects();
 
             var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: self.projectArray,
-            datasets: [{
-                label: 'Hours in Project',
-                data: self.totalHoursArray,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: self.projectArray,
+                    datasets: [{
+                        label: 'Hours',
+                        data: self.totalHoursArray,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: { title: {
+                    display: true,
+                    text: 'Hours per Project',
+                    fontSize: 40,
+                },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
                     }
-                }]
-            }
-        }
-    }); // end canvas view
-
-
-
-
-        });    
+                }
+            }); // end canvas view
+        });  // end HomeService.getProjects
     } // end getProjects
 
     self.getProjects();
